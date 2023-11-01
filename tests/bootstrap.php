787682e06638b73d05c09db8633f6eb8d46dfc0c
Ali Mahdian <a.mahdian@digikala.com>
2023-11-01 13:37:44 +0330

    run commands for db test in when eunning phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,5 +7,17 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env.test');
 }
+
+passthru(sprintf(
+    'php "%s/../bin/console" -env=%s doctrine:database:drop --force', __DIR__, $_ENV['APP_ENV']
+));
+
+passthru(sprintf(
+    'php "%s/../bin/console" -env=%s doctrine:database:create',  __DIR__, $_ENV['APP_ENV']
+));
+
+passthru(sprintf(
+    'php "%s/../bin/console" -env=%s doctrine:schema:create', __DIR__, $_ENV['APP_ENV']
+));
